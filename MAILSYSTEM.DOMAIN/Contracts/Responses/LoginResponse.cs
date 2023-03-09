5f74e016d8616a023f9b680dfe3ee7df69870db8
gongora84 <104776654+RASCloud1@users.noreply.github.com>
2023-03-09 12:31:50 -0500

    remove CompanyInformation from login response

--- MAILSYSTEM.DOMAIN/Contracts/Responses/LoginResponse.cs
+++ MAILSYSTEM.DOMAIN/Contracts/Responses/LoginResponse.cs
@@ -1,12 +1,8 @@
-﻿using MAILSYSTEM.DOMAIN.Entities;
-
-namespace MAILSYSTEM.DOMAIN.Contracts.Responses;
+﻿namespace MAILSYSTEM.DOMAIN.Contracts.Responses;
 
 public class LoginResponse
 {
     public string ApiKey { get; set; }
 
     public bool LoginStatus { get; set; }
-
-    public Company CompanyInformation { get; set; }
 }
