69eeb7e04294b6235e9fef680032fbab01953b1e
gongora84 <104776654+RASCloud1@users.noreply.github.com>
2023-03-10 18:10:59 -0500

    adding mapper for mail job

---
+++ MAILSYSTEM.APPLICATION/Persistence/Mapper/MailJobMapper.cs
@@ -0,0 +1,19 @@
+﻿namespace MAILSYSTEM.APPLICATION.Persistence.Mapper;
+
+public static class MailJobMapper
+{
+    private static readonly Lazy<IMapper> Lazy = new Lazy<IMapper>(() =>
+    {
+        var config = new MapperConfiguration(cfg =>
+        {
+            cfg.ShouldMapProperty = p => p.GetMethod.IsPublic || p.GetMethod.IsAssembly;
+            cfg.AddProfile<MailJobMapperProfile>();
+        });
+
+        var mapper = config.CreateMapper();
+
+        return mapper;
+    });
+
+    public static IMapper Mapper => Lazy.Value;
+}
---
+++ MAILSYSTEM.APPLICATION/Persistence/Mapper/MailJobMapperProfile.cs
@@ -0,0 +1,25 @@
+﻿using MAILSYSTEM.DOMAIN.Contracts.Responses.States;
+
+namespace MAILSYSTEM.APPLICATION.Persistence.Mapper;
+
+public class MailJobMapperProfile : Profile
+{
+    public MailJobMapperProfile()
+    {
+        CreateMap<MailJobRequest, MailJob>();
+
+        CreateMap<State, StateResponse>().ReverseMap();
+
+        CreateMap<List<State>, AllStateResponse>()
+            .ForMember(
+                dest => dest.Items,
+                src => src.MapFrom(s => s.Select(
+                    t => new StateResponse()
+                    {
+                        Id = t.Id,
+                        StateAbbreviation = t.StateAbbreviation,
+                        StateDescription = t.StateDescription
+                    }).ToList()))
+            .ForMember(dest => dest.Count, src => src.MapFrom(s => s.Count));
+    }
+}
